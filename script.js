916a2a91dd26b64dff51255b0c2a1efb55cb374f
Tiago Rangel <70700766+tiagorangel2011@users.noreply.github.com>
2024-07-23 09:37:06 +0100

    Fix issue where extension would error out when on the Glitch website but not on the editor

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId, url }) => {
-	if (url.startsWith("https://glitch.com")) {
-		chrome.scripting.insertCSS({
-			target: { tabId },
-			files: [ "glitch.css" ]
-		});
-		chrome.scripting.executeScript({
-			target: { tabId },
-			files: [ "glitch.js" ],
-			world: "MAIN"
-		});
+	url = new URL(url);
+	if (!((url.hostname === "glitch.com") && ((url.pathname === "/edit/") || (url.pathname === "/edit")))) {
+		return;
 	}
+	chrome.scripting.insertCSS({
+		target: { tabId },
+		files: [ "glitch.css" ]
+	});
+	chrome.scripting.executeScript({
+		target: { tabId },
+		files: [ "glitch.js" ],
+		world: "MAIN"
+	});
 });
